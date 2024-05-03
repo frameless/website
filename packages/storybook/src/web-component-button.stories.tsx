87354572dce01a2aa039560fdcf92a5e2e75fb79
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-05-03 10:09:52 +0200

    refactor: some additional replacements of example to frameless

--- packages/storybook/src/web-component-button.stories.tsx
+++ packages/storybook/src/web-component-button.stories.tsx
@@ -1,11 +1,11 @@
 /* @license CC0-1.0 */
 
 import readme from '@frameless/components-css/button/README.md?raw';
-import { ExampleButton } from '@frameless/web-components-react';
+import { FramelessButton } from '@frameless/web-components-react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { PropsWithChildren } from 'react';
 
-const Button = ({ children }: PropsWithChildren<{}>) => <ExampleButton>{children}</ExampleButton>;
+const Button = ({ children }: PropsWithChildren<{}>) => <FramelessButton>{children}</FramelessButton>;
 
 const meta = {
   title: 'Web Component/Button',
@@ -40,5 +40,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  name: 'Example button',
+  name: 'Frameless button',
 };

2a8eb6ec390063785bd33cf9b5378dc5fcd6df66
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-05-22 10:29:52 +0200

    build: Card added to stories

---
+++ packages/storybook/src/css-card.stories.tsx
@@ -0,0 +1,42 @@
+/* @license CC0-1.0 */
+
+import '@frameless/components-css/card/index.scss';
+import readme from '@frameless/components-css/card/README.md?raw';
+import { Card } from '@frameless/components-react/src/Card';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'CSS Component/Card',
+  id: 'css-card',
+  component: Card,
+  argTypes: {
+    children: {
+      name: 'Content',
+      description: 'Card',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: 'Opslaan en verder',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof Card>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: 'Frameless Card',
+};

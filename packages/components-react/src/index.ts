31b776dead34d3cb6804643b19f37ea225d1ddc5
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-05-28 15:15:26 +0200

    build: add Logo component to index.ts

--- packages/components-react/src/index.ts
+++ packages/components-react/src/index.ts
@@ -1,3 +1,4 @@
-export { Heading1 } from '@utrecht/component-library-react';
 export { Card } from './Card';
 export { CardGroup } from './CardGroup';
+export { Heading1 } from '@utrecht/component-library-react';
+export { Logo } from './Logo';

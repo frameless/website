79aa6f7719a8406081e35f2034e2076603b54a5a
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-05-28 15:13:20 +0200

    fix: fix: add FramelessLogo + adjust Logo component structure to fix styling issues

--- packages/components-react/src/Logo/index.tsx
+++ packages/components-react/src/Logo/index.tsx
@@ -1,12 +1,15 @@
 // Logo component is imported from Utrecht, LogoImage and LogoText are Frameless components
 
 import { Logo as UtrechtLogo } from '@utrecht/component-library-react';
+import { FramelessLogo } from './FramelessLogo';
 import { LogoImage } from './LogoImage';
 import { LogoText } from './LogoText';
 
 export const Logo = () => (
-  <UtrechtLogo className="frameless-logo">
-    <LogoImage />
+  <FramelessLogo className="frameless-logo">
+    <UtrechtLogo>
+      <LogoImage />
+    </UtrechtLogo>
     <LogoText>Frameless</LogoText>
-  </UtrechtLogo>
+  </FramelessLogo>
 );

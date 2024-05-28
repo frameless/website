bccb54f404bbc83b878af84c70c571cab78545ca
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-05-28 10:07:35 +0200

    build: index card component

---
+++ packages/components-react/src/Card/index.tsx
@@ -0,0 +1,13 @@
+import { Heading2, Paragraph } from '@utrecht/component-library-react';
+import { Card } from './Card';
+
+<Card>
+  <Heading2>Design System</Heading2>
+  <Paragraph>Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken!</Paragraph>
+  <Paragraph>
+    Frameless heeft al aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design
+    System.
+  </Paragraph>
+</Card>;
+
+// export default Card;

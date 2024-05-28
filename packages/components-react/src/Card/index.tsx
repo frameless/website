3830cc12c40c78f8660f5fab227653b10647d2ac
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-05-28 16:18:49 +0200

    build: card index component

--- packages/components-react/src/Card/index.tsx
+++ packages/components-react/src/Card/index.tsx
@@ -1,13 +1,21 @@
-import { Heading2, Paragraph } from '@utrecht/component-library-react';
-import { Card } from './Card';
+import clsx from 'clsx';
+import { HTMLAttributes, PropsWithChildren } from 'react';
+import '@frameless/components-css/card/index.scss';
 
-<Card>
-  <Heading2>Design System</Heading2>
-  <Paragraph>Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken!</Paragraph>
-  <Paragraph>
-    Frameless heeft al aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design
-    System.
-  </Paragraph>
-</Card>;
+// ensures that this component is flexible, can accept standard HTML attributes, and conditionally applies styles based on the background prop.
+interface CardProps extends HTMLAttributes<HTMLElement> {
+  background?: boolean;
+}
 
-// export default Card;
+export const Card = ({ background, children, className, ...restProps }: PropsWithChildren<CardProps>) => (
+  <div
+    {...restProps}
+    className={clsx(['frameless-card-test'], background, ['card__illustration--background'], className)}
+  >
+    {children}
+  </div>
+);
+
+export const CardContent = (props: PropsWithChildren<HTMLAttributes<HTMLElement>>) => (
+  <div className={clsx(['frameless-card-content'])} {...props} />
+);

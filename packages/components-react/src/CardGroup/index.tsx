ec9c029c02f5a8f1e5c6405e815eaae71bd22632
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-06-03 13:51:02 +0200

    feat: added card group

---
+++ packages/components-react/src/CardGroup/index.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx';
+import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react';
+import '@frameless/components-css/card/index.scss';
+
+export type CardGroupProps = HTMLAttributes<HTMLDivElement>;
+
+export const CardGroup = forwardRef(
+  ({ children, className, ...restProps }: PropsWithChildren<CardGroupProps>, ref: ForwardedRef<HTMLDivElement>) => (
+    <div
+      {...restProps}
+      ref={ref}
+      className={clsx(
+        'frameless-card-group',
+
+        className,
+      )}
+    >
+      {children}
+    </div>
+  ),
+);
+
+CardGroup.displayName = 'CardGroup';

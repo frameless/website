bb0bdecad849fd135d53c90f5ad6b5092f371e43
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-06-03 14:18:35 +0200

    refactor: add dist/css to (utrecht)logo import

--- packages/components-react/src/Logo/index.tsx
+++ packages/components-react/src/Logo/index.tsx
@@ -1,7 +1,7 @@
 // Logo component is imported from Utrecht
 // LogoImage, LogoText and LogoWrapper are Frameless components
 
-import { Logo as UtrechtLogo } from '@utrecht/component-library-react';
+import { Logo as UtrechtLogo } from '@utrecht/component-library-react/dist/css-module';
 import { LogoImage } from './LogoImage';
 import { LogoText } from './LogoText';
 import { LogoWrapper } from './LogoWrapper';

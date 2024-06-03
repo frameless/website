e519e4faa650c0a00fa7fe2053a38be11e0c24e1
Yolijn <yolijn@frameless.io>
2024-06-03 13:00:25 +0200

    feat: add html-addon to storybook

--- packages/storybook/config/ParametersArgsDecorator.tsx
+++ packages/storybook/config/ParametersArgsDecorator.tsx
@@ -4,7 +4,7 @@ export const ParametersArgsDecorator: Decorator = (Story, context) => {
   context.parameters['args'] = context.args;
 
   return (
-    <div className="frameless-theme">
+    <div className="frameless-theme" id="frameless-root">
       <Story />
     </div>
   );

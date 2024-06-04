c31e3822d50d708e8c87c448d9038b70a83b3b6f
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-06-04 14:11:12 +0200

    build: added children (Heading and Paragraph) to card and cardgroup components

--- packages/storybook/src/css-card.stories.tsx
+++ packages/storybook/src/css-card.stories.tsx
@@ -4,6 +4,7 @@ import '@frameless/components-css/card/index.scss';
 import readme from '@frameless/components-css/card/README.md?raw';
 import { Card } from '@frameless/components-react/src/Card';
 import type { Meta, StoryObj } from '@storybook/react';
+import { Heading3, Paragraph } from '@utrecht/component-library-react';
 
 const meta = {
   title: 'CSS Component/Card',
@@ -38,5 +39,16 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <Heading3>Design Systems</Heading3>
+        <Paragraph>
+          Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
+          aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
+        </Paragraph>
+      </>
+    ),
+  },
   name: 'Frameless Card',
 };
--- packages/storybook/src/css-cardgroup.stories.tsx
+++ packages/storybook/src/css-cardgroup.stories.tsx
@@ -1,13 +1,15 @@
 /* @license CC0-1.0 */
 
 import '@frameless/components-css/card/index.scss';
-import readme from '@frameless/components-css/card/README.md?raw';
+import readme from '@frameless/components-css/card-group/README.md?raw';
+import { Card } from '@frameless/components-react/src/Card';
 import { CardGroup } from '@frameless/components-react/src/CardGroup';
 import type { Meta, StoryObj } from '@storybook/react';
+import { Heading3, Paragraph } from '@utrecht/component-library-react';
 
 const meta = {
   title: 'CSS Component/CardGroup',
-  id: 'css-card',
+  id: 'css-card-group',
   component: CardGroup,
   argTypes: {
     children: {
@@ -38,5 +40,25 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <Card>
+          <Heading3>Design Systems</Heading3>
+          <Paragraph>
+            Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
+            aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
+          </Paragraph>
+        </Card>
+        <Card>
+          <Heading3>Design Systems</Heading3>
+          <Paragraph>
+            Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
+            aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
+          </Paragraph>
+        </Card>
+      </>
+    ),
+  },
   name: 'Frameless CardGroup',
 };

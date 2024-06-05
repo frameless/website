1fb16a759fcd3fec205dbe8e8698be72376a5ef5
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-06-05 14:26:51 +0200

    feat:

--- packages/storybook/src/css-card.stories.tsx
+++ packages/storybook/src/css-card.stories.tsx
@@ -4,7 +4,7 @@ import '@frameless/components-css/card/index.scss';
 import readme from '@frameless/components-css/card/README.md?raw';
 import { Card } from '@frameless/components-react/src/Card';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Heading3, Paragraph } from '@utrecht/component-library-react';
+import { Heading2, Heading3, Heading4, Heading5, Heading6, Paragraph } from '@utrecht/component-library-react';
 
 const meta = {
   title: 'CSS Component/Card',
@@ -42,6 +42,52 @@ export const Default: Story = {
   args: {
     children: (
       <>
+        <Paragraph>
+          Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
+          aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
+        </Paragraph>
+      </>
+    ),
+  },
+};
+
+export const LongHeading: Story = {
+  args: {
+    children: (
+      <>
+        <Heading3>The quick brown fox jumps over the lazy dog</Heading3>
+        <Paragraph>
+          Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
+          aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
+        </Paragraph>
+      </>
+    ),
+  },
+};
+
+export const Empty: Story = {
+  args: {
+    children: '',
+  },
+};
+
+export const WithHeading2: Story = {
+  args: {
+    children: (
+      <>
+        <Heading2>Design Systems</Heading2>
+        <Paragraph>
+          Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
+          aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
+        </Paragraph>
+      </>
+    ),
+  },
+};
+export const WithHeading3: Story = {
+  args: {
+    children: (
+      <>
         <Heading3>Design Systems</Heading3>
         <Paragraph>
           Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
@@ -50,5 +96,44 @@ export const Default: Story = {
       </>
     ),
   },
-  name: 'Frameless Card',
+};
+export const WithHeading4: Story = {
+  args: {
+    children: (
+      <>
+        <Heading4>Design Systems</Heading4>
+        <Paragraph>
+          Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
+          aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
+        </Paragraph>
+      </>
+    ),
+  },
+};
+export const WithHeading5: Story = {
+  args: {
+    children: (
+      <>
+        <Heading5>Design Systems</Heading5>
+        <Paragraph>
+          Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
+          aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
+        </Paragraph>
+      </>
+    ),
+  },
+};
+
+export const WithHeading6: Story = {
+  args: {
+    children: (
+      <>
+        <Heading6>Design Systems</Heading6>
+        <Paragraph>
+          Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
+          aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
+        </Paragraph>
+      </>
+    ),
+  },
 };

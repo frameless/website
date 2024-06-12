1c62a775910416d7e2e611f51c1bc20c4342b245
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-06-12 09:58:21 +0200

    refactor: added heading in args and paragraph in children

--- packages/storybook/src/css-card.stories.tsx
+++ packages/storybook/src/css-card.stories.tsx
@@ -1,10 +1,8 @@
-/* @license CC0-1.0 */
-
 import '@frameless/components-css/card/index.scss';
 import readme from '@frameless/components-css/card/README.md?raw';
 import { Card } from '@frameless/components-react/src/Card';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Heading2, Heading3, Heading4, Heading5, Heading6, Paragraph } from '@utrecht/component-library-react';
+import { Paragraph } from '@utrecht/component-library-react';
 
 const meta = {
   title: 'CSS Component/Card',
@@ -53,9 +51,9 @@ export const Default: Story = {
 
 export const LongHeading: Story = {
   args: {
+    heading: 'The quick brown fox jumps over the lazy dog',
     children: (
       <>
-        <Heading3>The quick brown fox jumps over the lazy dog</Heading3>
         <Paragraph>
           Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
           aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
@@ -73,9 +71,9 @@ export const Empty: Story = {
 
 export const WithHeading2: Story = {
   args: {
+    heading: 'Design Systems',
     children: (
       <>
-        <Heading2>Design Systems</Heading2>
         <Paragraph>
           Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
           aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
@@ -86,9 +84,10 @@ export const WithHeading2: Story = {
 };
 export const WithHeading3: Story = {
   args: {
+    heading: 'Design Systems',
+    headingLevel: 3,
     children: (
       <>
-        <Heading3>Design Systems</Heading3>
         <Paragraph>
           Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
           aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
@@ -99,9 +98,10 @@ export const WithHeading3: Story = {
 };
 export const WithHeading4: Story = {
   args: {
+    heading: 'Design Systems',
+    headingLevel: 4,
     children: (
       <>
-        <Heading4>Design Systems</Heading4>
         <Paragraph>
           Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
           aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
@@ -112,9 +112,10 @@ export const WithHeading4: Story = {
 };
 export const WithHeading5: Story = {
   args: {
+    heading: 'Design Systems',
+    headingLevel: 5,
     children: (
       <>
-        <Heading5>Design Systems</Heading5>
         <Paragraph>
           Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
           aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.
@@ -126,9 +127,10 @@ export const WithHeading5: Story = {
 
 export const WithHeading6: Story = {
   args: {
+    heading: 'Design Systems',
+    headingLevel: 6,
     children: (
       <>
-        <Heading6>Design Systems</Heading6>
         <Paragraph>
           Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken! Frameless heeft al
           aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan NL Design System.

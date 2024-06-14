e55766ee34e4d877642da005240123e17a203ac5
Yolijn <mail@yolijn.com>
2024-06-14 12:18:06 +0000

    Deploying to gh-pages from @ frameless/website@b7117264837736e7b8deb6c75113135fb0f60741 🚀

---
+++ assets/WithTooltip-KJL26V4Q-BnePbf89.js
@@ -0,0 +1,1 @@
+import{W as e,W as h,b as l}from"./chunk-HLWAVYOI-B4NZmwVE.js";import"./iframe-DbI7jdFe.js";import"../sb-preview/runtime.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./react-18-DhHUPTI3.js";import"./extends-CCbyfPlC.js";import"./index-ChwMrSJI.js";export{e as WithToolTipState,h as WithTooltip,l as WithTooltipPure};
---
+++ assets/css-logo.stories-D3jRe1c2.js
@@ -0,0 +1,9 @@
+import{L as p,r as c}from"./index-DvCl45Np.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D4gnSVIr.js";import"./objectDestructuringEmpty-B41EKLkj.js";import"./extends-CCbyfPlC.js";import"./clsx-B-dksMZM.js";/* @license CC0-1.0 */const F={title:"CSS Component/Logo",id:"css-logo",component:p,argTypes:{children:{name:"Content",description:"Logo",type:{name:"string",required:!0},defaultValue:""}},args:{},tags:["autodocs"],parameters:{docs:{description:{component:c}}}},e={name:"Frameless Logo",args:{},parameters:{}},r={name:"Frameless Logo Small",args:{},parameters:{}};var a,o,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: 'Frameless Logo',
+  args: {},
+  parameters: {}
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var t,m,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: 'Frameless Logo Small',
+  args: {},
+  parameters: {}
+}`,...(n=(m=r.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};const C=["Default","LogoSmall"];export{e as Default,r as LogoSmall,C as __namedExportsOrder,F as default};

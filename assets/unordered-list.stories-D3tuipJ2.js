139bc2d4f030fc15e71fca61c5e3297a579ed1ab
Yolijn <mail@yolijn.com>
2024-06-14 12:31:21 +0000

    Deploying to gh-pages from @ frameless/website@de1c9138d9afa2c0e3eeabd0127d649ad32a9d8e 🚀

---
+++ assets/unordered-list.stories-D3tuipJ2.js
@@ -0,0 +1,29 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{U as s,f as n}from"./index-Bhx1S-Xd.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./objectDestructuringEmpty-ENdUJuAZ.js";import"./extends-CF3RwP-h.js";const p={title:"CSS Component/Unordered list",id:"css-utrecht-unordered-list",component:s,argTypes:{children:{name:"Content",type:{name:"string",required:!0},defaultValue:""}},args:{children:""},tags:["autodocs"],parameters:{docs:{description:{}}}},t={name:"Unordered list",args:{children:[e.jsx(n,{children:"NL Design System: Yolijn en Robbert zijn sinds 2020 verantwoordelijk voor de architectuur,community en groei van NL Design System."}),e.jsx(n,{children:"Utrecht Design System: meer dan 80 componenten ontwikkeld voor Utrecht in verschillende technieken (CSS, HTML, React, Angular, Vue en als Web Component). Deze componenten worden inmiddels ook veel gebruikt door uiteenlopende projecten in de NL Design System community."}),e.jsx(n,{children:"Den Haag Design System: begeleiding van het design system team, en leveranciers begeleiden met open source bouwen volgens de NL Design System architectuur."}),e.jsx(n,{children:"Digitaal Loket van gemeente Utrecht: ontwikkeling van Strapi CMS voor de Producten en Diensten Catalogus, en de nieuwe website ontwikkelen als headless frontend op basis van NL Design System. Dit project wordt in de loop van 2024 zichtbaar op utrecht.nl."}),e.jsx(n,{children:"Huwelijksplanner voor gemeente Utrecht: front-end ontwikkeling Huwelijksplanner CMS op basis van NL Design System componenten."}),e.jsx(n,{children:"Gemeente Voorbeeld: ontwikkeling van demo-website waar Vereniging van Nederlandse Gemeenten (VNG) gebruikerstesten doet, om gemeenten op weg te helpen met voldoen aan de WMEBV met NL Design System."}),e.jsx(n,{children:"OpenCatalogi.nl - migratie naar NL Design System: migratie van codebase naar React componenten uit de NL Design System community."})]}};var r,o,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Unordered list',
+  args: {
+    children: [<UnorderedListItem>
+        NL Design System: Yolijn en Robbert zijn sinds 2020 verantwoordelijk voor de architectuur,community en groei van
+        NL Design System.
+      </UnorderedListItem>, <UnorderedListItem>
+        Utrecht Design System: meer dan 80 componenten ontwikkeld voor Utrecht in verschillende technieken (CSS, HTML,
+        React, Angular, Vue en als Web Component). Deze componenten worden inmiddels ook veel gebruikt door
+        uiteenlopende projecten in de NL Design System community.
+      </UnorderedListItem>, <UnorderedListItem>
+        Den Haag Design System: begeleiding van het design system team, en leveranciers begeleiden met open source
+        bouwen volgens de NL Design System architectuur.
+      </UnorderedListItem>, <UnorderedListItem>
+        Digitaal Loket van gemeente Utrecht: ontwikkeling van Strapi CMS voor de Producten en Diensten Catalogus, en de
+        nieuwe website ontwikkelen als headless frontend op basis van NL Design System. Dit project wordt in de loop van
+        2024 zichtbaar op utrecht.nl.
+      </UnorderedListItem>, <UnorderedListItem>
+        Huwelijksplanner voor gemeente Utrecht: front-end ontwikkeling Huwelijksplanner CMS op basis van NL Design
+        System componenten.
+      </UnorderedListItem>, <UnorderedListItem>
+        Gemeente Voorbeeld: ontwikkeling van demo-website waar Vereniging van Nederlandse Gemeenten (VNG)
+        gebruikerstesten doet, om gemeenten op weg te helpen met voldoen aan de WMEBV met NL Design System.
+      </UnorderedListItem>, <UnorderedListItem>
+        OpenCatalogi.nl - migratie naar NL Design System: migratie van codebase naar React componenten uit de NL Design
+        System community.
+      </UnorderedListItem>]
+  }
+}`,...(i=(o=t.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,p as default};

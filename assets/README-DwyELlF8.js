bc7a4e1f601710e3f3295abb76496d280cc19631
Yolijn <mail@yolijn.com>
2024-06-29 15:47:49 +0000

    Deploying to gh-pages from @ frameless/website@3723289f87c92421621e431462c3923d3f610f5f 🚀

---
+++ assets/README-DwyELlF8.js
@@ -0,0 +1,4 @@
+const e=`<!-- @license CC0-1.0 -->
+
+# Frameless CardGroup component
+`;export{e as r};
---
+++ assets/homepage.stories-CsirQIAk.js
@@ -0,0 +1,103 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{C as r}from"./index-Ckrq33cK.js";import{C as g}from"./index-BEmDXiN8.js";import{L as c}from"./index-DJI6WXKw.js";import{D as h,g as p,h as k,a,P as s,L as n,U as j,f as t}from"./index-D7ShSdLM.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./objectDestructuringEmpty-ENdUJuAZ.js";import"./extends-CF3RwP-h.js";import"./clsx-B-dksMZM.js";const N={title:"Template/Homepage",id:"template-homepage",parameters:{}},i={render:m=>e.jsx(h,{children:e.jsx(p,{className:"utrecht-page__content",children:e.jsx(k,{children:e.jsx("div",{...m,className:"frameless-page-container",children:e.jsxs("div",{className:"frameless-page",children:[e.jsx(c,{}),e.jsxs("section",{className:"frameless-section__about",children:[e.jsx(a,{children:"Wij Maken"}),e.jsxs(g,{children:[e.jsxs(r,{headingLevel:3,heading:"Design Systems",children:[e.jsx(s,{children:"Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken!"}),e.jsxs(s,{children:["Frameless heeft al aan meerdere white-label design systems mogen werken en is trots op onze bijdrage aan ",e.jsx(n,{href:"https://nldesignsystem.nl",children:"NL Design System"})]})]}),e.jsx(r,{headingLevel:3,heading:"Front-ends",children:e.jsx(s,{children:"Wij helpen graag als technisch partner bij het bouwen van toegankelijke en gebruiksvriendelijke web applicaties."})})]})]}),e.jsxs("section",{className:"frameless-section__contact",children:[e.jsx(a,{children:"Kennis maken?"}),e.jsxs(s,{children:["Mail ons op",e.jsx(n,{href:"mailto:hello@frameless.io",children:" hello@frameless.io"}),", dan spreken we snel een keer af!"]})]}),e.jsxs("section",{className:"frameless-section__projecten",children:[e.jsx(a,{children:"Goed voor elkaar"}),e.jsx(s,{children:"Afgelopen tijd hebben Robbert, Yolijn, Ali, Savi, Rowan, Tessa, Marwa, Angela en Bryan aan veel leuke projecten gewerkt:"}),e.jsxs(j,{children:[e.jsxs(t,{children:[e.jsx(n,{href:"https://nldesignsystem.nl/",children:"NL Design System"}),": Yolijn en Robbert zijn sinds 2020 verantwoordelijk voor de architectuur, community en groei van NL Design System."]}),e.jsxs(t,{children:[e.jsx(n,{href:"https://nl-design-system.github.io/utrecht/storybook/",children:"Utrecht Design System"}),": meer dan 80 componenten ontwikkeld voor Utrecht in verschillende technieken: als CSS, HTML, React, Angular, Vue en als Web Component. Deze componenten worden inmiddels ook veel gebruikt door uiteenlopende projecten in de NL Design System community."]}),e.jsxs(t,{children:[e.jsx(n,{href:"https://nl-design-system.github.io/denhaag/",children:" Den Haag Design System"}),": begeleiding van het design system team, en leveranciers begeleiden met open source bouwen volgens de NL Design System architectuur."]}),e.jsxs(t,{children:[e.jsx(n,{href:"https://pki.utrecht.nl/",children:"Digitaal Loket van gemeente Utrecht"}),": ontwikkeling van Strapi CMS voor de Producten en Diensten Catalogus, en de nieuwe website ontwikkelen als headless frontend op basis van NL Design System. Dit project wordt in de loop van 2024 zichtbaar op utrecht.nl."]}),e.jsx(t,{children:"Huwelijksplanner voor gemeente Utrecht: front-end ontwikkeling Huwelijksplanner CMS op basis van NL Design System componenten."}),e.jsxs(t,{children:[e.jsx(n,{href:"https://gemeentevoorbeeld.nl/",children:"gemeente Voorbeeld"}),": ontwikkeling van demo-website waar Vereniging van Nederlandse Gemeenten (VNG) gebruikerstesten doet, om gemeenten op weg te helpen met voldoen aan de ",e.jsx(n,{href:"https://www.digitaleoverheid.nl/overzicht-van-alle-onderwerpen/wetgeving/wet-modernisering-elektronisch-bestuurlijk-verkeer/",children:"WMEBV"})," met NL Design System."]}),e.jsxs(t,{children:[e.jsx(n,{href:"https://opencatalogi.nl/",children:"OpenCatalogi.nl — migratie naar NL Design System"}),": migratie van codebase naar React componenten uit de NL Design System community."]}),e.jsxs(t,{children:[e.jsx(n,{href:"https://opencatalogi.nl/",children:"Huisstijl van gemeente Rotterdam voor OpenCatalogi.nl"}),": ontwikkeling van NL Design System thema zodat OpenCatalogi kan overstappen op NL Design System."]})]})]})]})})})})})};var o,l,d;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => <Document>
+      <Page className="utrecht-page__content">
+        <PageContent>
+          <div {...args} className="frameless-page-container">
+            <div className="frameless-page">
+              <Logo />
+              <section className="frameless-section__about">
+                <Heading2>Wij Maken</Heading2>
+
+                <CardGroup>
+                  <Card headingLevel={3} heading="Design Systems">
+                    <Paragraph>
+                      Herbruikbare componenten onafhankelijk van huisstijl, daar mag je ons voor wakker maken!
+                    </Paragraph>
+                    <Paragraph>
+                      Frameless heeft al aan meerdere white-label design systems mogen werken en is trots op onze
+                      bijdrage aan <Link href="https://nldesignsystem.nl">NL Design System</Link>
+                    </Paragraph>
+                  </Card>
+
+                  <Card headingLevel={3} heading="Front-ends">
+                    <Paragraph>
+                      Wij helpen graag als technisch partner bij het bouwen van toegankelijke en gebruiksvriendelijke
+                      web applicaties.
+                    </Paragraph>
+                  </Card>
+                </CardGroup>
+              </section>
+
+              <section className="frameless-section__contact">
+                <Heading2>Kennis maken?</Heading2>
+                <Paragraph>
+                  Mail ons op
+                  <Link href="mailto:hello@frameless.io"> hello@frameless.io</Link>, dan spreken we snel een keer af!
+                </Paragraph>
+              </section>
+
+              <section className="frameless-section__projecten">
+                <Heading2>Goed voor elkaar</Heading2>
+                <Paragraph>
+                  Afgelopen tijd hebben Robbert, Yolijn, Ali, Savi, Rowan, Tessa, Marwa, Angela en Bryan aan veel leuke
+                  projecten gewerkt:
+                </Paragraph>
+
+                <UnorderedList>
+                  <UnorderedListItem>
+                    <Link href="https://nldesignsystem.nl/">NL Design System</Link>: Yolijn en Robbert zijn sinds 2020
+                    verantwoordelijk voor de architectuur, community en groei van NL Design System.
+                  </UnorderedListItem>
+
+                  <UnorderedListItem>
+                    <Link href="https://nl-design-system.github.io/utrecht/storybook/">Utrecht Design System</Link>:
+                    meer dan 80 componenten ontwikkeld voor Utrecht in verschillende technieken: als CSS, HTML, React,
+                    Angular, Vue en als Web Component. Deze componenten worden inmiddels ook veel gebruikt door
+                    uiteenlopende projecten in de NL Design System community.
+                  </UnorderedListItem>
+
+                  <UnorderedListItem>
+                    <Link href="https://nl-design-system.github.io/denhaag/"> Den Haag Design System</Link>: begeleiding
+                    van het design system team, en leveranciers begeleiden met open source bouwen volgens de NL Design
+                    System architectuur.
+                  </UnorderedListItem>
+
+                  <UnorderedListItem>
+                    <Link href="https://pki.utrecht.nl/">Digitaal Loket van gemeente Utrecht</Link>: ontwikkeling van
+                    Strapi CMS voor de Producten en Diensten Catalogus, en de nieuwe website ontwikkelen als headless
+                    frontend op basis van NL Design System. Dit project wordt in de loop van 2024 zichtbaar op
+                    utrecht.nl.
+                  </UnorderedListItem>
+
+                  <UnorderedListItem>
+                    Huwelijksplanner voor gemeente Utrecht: front-end ontwikkeling Huwelijksplanner CMS op basis van NL
+                    Design System componenten.
+                  </UnorderedListItem>
+
+                  <UnorderedListItem>
+                    <Link href="https://gemeentevoorbeeld.nl/">gemeente Voorbeeld</Link>: ontwikkeling van demo-website
+                    waar Vereniging van Nederlandse Gemeenten (VNG) gebruikerstesten doet, om gemeenten op weg te helpen
+                    met voldoen aan de&#160;
+                    <Link href="https://www.digitaleoverheid.nl/overzicht-van-alle-onderwerpen/wetgeving/wet-modernisering-elektronisch-bestuurlijk-verkeer/">
+                      WMEBV
+                    </Link>
+                    &#160;met NL Design System.
+                  </UnorderedListItem>
+
+                  <UnorderedListItem>
+                    <Link href="https://opencatalogi.nl/">OpenCatalogi.nl — migratie naar NL Design System</Link>:
+                    migratie van codebase naar React componenten uit de NL Design System community.
+                  </UnorderedListItem>
+
+                  <UnorderedListItem>
+                    <Link href="https://opencatalogi.nl/">Huisstijl van gemeente Rotterdam voor OpenCatalogi.nl</Link>:
+                    ontwikkeling van NL Design System thema zodat OpenCatalogi kan overstappen op NL Design System.
+                  </UnorderedListItem>
+                </UnorderedList>
+              </section>
+            </div>
+          </div>
+        </PageContent>
+      </Page>
+    </Document>
+}`,...(d=(l=i.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const C=["Default"];export{i as Default,C as __namedExportsOrder,N as default};

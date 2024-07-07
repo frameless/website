c996fa33497be865302b04337cc4fa2fa6758fc7
Yolijn <yolijn@frameless.io>
2024-07-07 17:08:22 +0200

    build: ensure build of html and css files to dist directory

---
+++ apps/next.frameless.io/rollup.config.mjs
@@ -0,0 +1,25 @@
+import postcss from "rollup-plugin-postcss";
+import discardDuplicates from "postcss-discard-duplicates";
+import copy from "rollup-plugin-copy";
+
+export default [
+  {
+    input: "src/index.scss",
+    output: {
+      dir: "./dist",
+      sourcemap: false,
+      format: "esm",
+      compact: true,
+    },
+    plugins: [
+      postcss({
+        extensions: [".css", ".scss"],
+        plugins: [discardDuplicates()],
+        extract: true,
+      }),
+      copy({
+        targets: [{ src: "src/index.html", dest: "dist" }],
+      }),
+    ],
+  },
+];

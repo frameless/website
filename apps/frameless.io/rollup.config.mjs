ddd7f7ed85307b45797eb4022a4ae9c022219138
Yolijn <yolijn@frameless.io>
2024-07-14 11:13:47 +0200

    chore: improve lighthouse scores by adding better rollup configuration

--- apps/frameless.io/rollup.config.mjs
+++ apps/frameless.io/rollup.config.mjs
@@ -1,6 +1,8 @@
 import postcss from "rollup-plugin-postcss";
 import discardDuplicates from "postcss-discard-duplicates";
 import copy from "rollup-plugin-copy";
+import gzipPlugin from "rollup-plugin-gzip";
+import purgecss from "@fullhuman/postcss-purgecss";
 
 export default [
   {
@@ -14,12 +16,22 @@ export default [
     plugins: [
       postcss({
         extensions: [".css", ".scss"],
-        plugins: [discardDuplicates()],
+        minimize: true,
+        plugins: [
+          discardDuplicates(),
+          purgecss({
+            content: ["./src/index.html"],
+          }),
+        ],
         extract: true,
       }),
       copy({
-        targets: [{ src: "src/index.html", dest: "dist" }],
+        targets: [
+          { src: "src/index.html", dest: "dist" },
+          { src: "src/weverssingel.svg", dest: "dist" },
+        ],
       }),
+      gzipPlugin(),
     ],
   },
 ];

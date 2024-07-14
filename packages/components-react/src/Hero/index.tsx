7d35bc61ba3da77d24dab080a7b484b64eabc4f2
Yolijn <yolijn@frameless.io>
2024-07-14 11:02:16 +0200

    feat: add hero component

---
+++ packages/components-react/src/Hero/index.tsx
@@ -0,0 +1,14 @@
+import clsx from 'clsx';
+import { HTMLAttributes, PropsWithChildren } from 'react';
+import '@frameless/components-css/hero/index.scss';
+import { PageContent } from '../PageContent';
+
+interface HeroProps extends HTMLAttributes<HTMLDivElement> {}
+
+export const Hero = ({ children, className, ...restProps }: PropsWithChildren<HeroProps>) => (
+  <div {...restProps} className={clsx('frameless-hero', className)}>
+    <PageContent>
+      <div className={clsx('frameless-hero__content')}>{children}</div>
+    </PageContent>
+  </div>
+);

7690d397d47bedebffbad86ce7ecb785b2326864
Yolijn <yolijn@frameless.io>
2024-07-14 13:05:01 +0200

    build: add hashed css

--- apps/frameless.io/rollup.config.mjs
+++ apps/frameless.io/rollup.config.mjs
@@ -1,19 +1,18 @@
 import postcss from "rollup-plugin-postcss";
 import discardDuplicates from "postcss-discard-duplicates";
-import copy from "rollup-plugin-copy";
-import gzipPlugin from "rollup-plugin-gzip";
 import purgecss from "@fullhuman/postcss-purgecss";
+import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
+import copy from "rollup-plugin-copy";
 
 export default [
   {
     input: "src/index.scss",
     output: {
       dir: "./dist",
-      sourcemap: false,
-      format: "esm",
       compact: true,
     },
     plugins: [
+      // Transform SCSS to one CSS file with only used CSS
       postcss({
         extensions: [".css", ".scss"],
         minimize: true,
@@ -25,13 +24,25 @@ export default [
         ],
         extract: true,
       }),
+      // Copy html file to enable replacing of the scss import to the minified css file created by the previous step
+      // This enables us to have the html plugin automatically build a hash for the minified css file in the next step
       copy({
         targets: [
-          { src: "src/index.html", dest: "dist" },
-          { src: "src/weverssingel.svg", dest: "dist" },
+          {
+            src: "./src/index.html",
+            dest: "./tmp",
+            transform: (contents) =>
+              contents.toString().replace("./index.scss", "../dist/index.css"),
+          },
         ],
       }),
-      gzipPlugin(),
     ],
+  },
+  // Use the html file with a reference to the minified css
+  // And create a hashed CSS reference to ensure new versions will reach all users regardless of cache settings
+  {
+    input: "./tmp/index.html",
+    output: { dir: "./dist" },
+    plugins: [html()],
   },
 ];

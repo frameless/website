57b145a7309cadd6541e891c2544e1d3d334047b
Yolijn <yolijn@frameless.io>
2024-10-13 21:39:16 +0200

    fix: page title

---
+++ apps/frameless.io/pages/index/+config.ts
@@ -0,0 +1,5 @@
+export default {
+  title: 'Frameless: technisch partner voor toegankelijke, gebruiksvriendelijke webapplicaties.',
+  description:
+    'Frameless ontzorgt bij implementatie, beheer, ondersteuning en toegankelijkheid. Ervaren in transitie van bestaande systemen naar gebruiksvriendelijke, onderhoudbare oplossingen met de NL Design System aanpak en Common Ground principes. Ook voeren we WCAG-EM audits uit en maken we toegankelijkheidsverklaringen.',
+};

d91789e2bc38a331dbe3e1a983a019075bc900ba
Yolijn <yolijn@frameless.io>
2024-10-13 21:39:16 +0200

    fix: page title

--- apps/frameless.io/renderer/getPageTitle.ts
+++ apps/frameless.io/renderer/getPageTitle.ts
@@ -7,6 +7,6 @@ export const getPageTitle = (pageContext: PageContext): string => {
     // Title defined statically by /pages/some-page/+title.js (or by `export default { title }` in /pages/some-page/+config.js)
     // The setting 'pageContext.config.title' is a custom setting we defined at ./+config.ts
     pageContext.config.title ||
-    'Vike Demo';
+    'Frameless';
   return title;
 };
